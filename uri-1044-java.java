4279508090651ea3f23df58cfd7ffaa24de31c56
Marina Calheira de Oliveira <74269921+nina0-0nina@users.noreply.github.com>
2021-06-25 12:11:35 -0300

    Add files via upload

---
+++ uri-1044-java.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner ent = new Scanner(System.in);
+		int A = ent.nextInt();
+		int B = ent.nextInt();
+	        if (A % B == 0||B % A == 0){
+	           System.out.println("Sao Multiplos");
+	        }
+	        else {
+	            System.out.println("Nao sao Multiplos");
+	        }
+	}
+}
